c6bb85a4717c8d01539fe681c622d76138363b87
Westley <WestleyArgentum@gmail.com>
2013-09-29 20:09:30 -0700

    CommonLog definition, rendering

--- src/Lumberjack.jl
+++ src/Lumberjack.jl
@@ -1,13 +1,46 @@
 
 module Lumberjack
 
+import Base.show
+using Datetime
+
+# -------
+
+abstract LogFmt
+
+# -------
+
+type CommonLog <: LogFmt
+    remotehost
+    rfc931
+    authuser
+    #date
+    request
+    status
+    bytes
+
+    CommonLog() = new()
+    CommonLog(remotehost, rfc931, authuser, request, status, bytes) = new(remotehost, rfc931, authuser, request, status, bytes)
+end
+
+function show(io::IO, l::CommonLog)
+    date = now()
+    print(io, "$(l.remotehost) $(l.rfc931) $(l.authuser) [$date] \"$(l.request)\" $(l.status) $(l.bytes)")
+end
+
+# -------
+
+type ExtendedLog <: LogFmt
+end
+
 # -------
 
 type LumberMill
     mode    # debug, release
     io::IO
+    fmt::LogFmt
 
-    LumberMill() = new("debug", Base.STDOUT)
+    LumberMill() = new("debug", Base.STDOUT, CommonLog())
     LumberMill(mode::String, io::IO) = new(mode, io)
 end
 
--- test/Lumberjack.jl
+++ test/Lumberjack.jl
@@ -2,3 +2,6 @@
 
 require("../src/Lumberjack.jl")
 
+l = Lumberjack.CommonLog("127.0.0.1", "bob", "bigbob", "GET /hotdog", 200, 42)
+
+show(l)
