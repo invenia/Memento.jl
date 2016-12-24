1162aa67a4877264d724e8da54c4e04ac8bd7b61
Rory-Finnegan <rory.finnegan@gmail.com>
2016-12-24 15:03:09 -0600

    Updated the quickstart image and script.

--- docs/quickstart.jl
+++ docs/quickstart.jl
@@ -1,9 +1,8 @@
 # Copy and paste these line into a REPL and screen capture to produce the
 # sample usage image.
 using Memento
-basic_config("debug"; fmt="[{date} | {level} | {name}]: {msg}")
-logger = get_logger(current_module())
-debug(logger, "Something that won't get logged.")
+logger = basic_config("debug"; fmt="[{level} | {name}]: {msg}")
+debug(logger, "Something to help you track down a bug.")
 info(logger, "Something you might want to know.")
 notice(logger, "This is probably pretty important.")
 warn(logger, "This might cause an error.")
@@ -12,3 +11,8 @@ error(logger, "Something that should throw an error.")
 critical(logger, "The world is exploding")
 alert(logger, "Alert the sysadmin if they're still sleeping.")
 emergency(logger, "The world has exploded and you probably brought down the server.")
+child_logger = get_logger("Foo.bar")
+set_level(child_logger, "warn")
+add_handler(child_logger, DefaultHandler(tempname(), DefaultFormatter("[{date} | {level} | {name}]: {msg}")))
+debug(child_logger, "Something that should only be printed to STDOUT on the root_logger.")
+warn(child_logger, "Warning to STDOUT and the log file.")
