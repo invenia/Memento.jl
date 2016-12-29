4686e8abe3165b6c621326698a57584d37ff01dd
Rory Finnegan <Rory-Finnegan@users.noreply.github.com>
2016-12-28 23:56:42 -0600

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -23,8 +23,9 @@ makedocs(
 )
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/invenia/Memento.jl.git",
     julia  = "0.5",
-    osname = "osx"
+    target = "build",
+    deps = nothing,
+    make = nothing,
 )
