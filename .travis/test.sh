ec75d34e1f00e7bc85d466e87e27e406063747d3
Curtis Vogt <curtis.vogt@gmail.com>
2017-05-01 15:46:12 -0500

    Create userimg test

--- .travis/test.sh
+++ .travis/test.sh
@@ -14,4 +14,13 @@ elif [[ "$TEST_TYPE" == "bench" ]]; then
     export MEMENTO_CURR_COMMIT="HEAD"
     export MEMENTO_BASE_COMMIT="origin/master"
     julia -e 'Pkg.test("Memento"; coverage=true)'
+elif [[ "$TEST_TYPE" == "userimage" ]]; then
+    julia -e '
+        LIB_PATH = abspath(Base.JULIA_HOME, Base.LIBDIR)
+        sysimg_lib = joinpath(LIB_PATH, "julia", "sys.$(Libdl.dlext)")
+        userimg_o = "userimg.o"
+        userimg_lib = "userimg.$(Libdl.dlext)"
+        run(`$(Base.julia_cmd()) --output-o $userimg_o --sysimage $sysimg_lib --startup-file=no -e "using Memento; logger = get_logger(\"Test\")"`)
+        run(`cc -shared -o $userimg_lib $userimg_o -ljulia -L$LIB_PATH`)
+    '
 fi
