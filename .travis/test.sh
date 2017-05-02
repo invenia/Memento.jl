791af3290cf94597128928f6a0f14e69d4384d2e
Curtis Vogt <curtis.vogt@gmail.com>
2017-05-02 15:53:03 -0500

    Create user image test (#23)
    * Create userimg test
    
    * [ci skip] Comment about user image test

--- .travis/test.sh
+++ .travis/test.sh
@@ -14,4 +14,15 @@ elif [[ "$TEST_TYPE" == "bench" ]]; then
     export MEMENTO_CURR_COMMIT="HEAD"
     export MEMENTO_BASE_COMMIT="origin/master"
     julia -e 'Pkg.test("Memento"; coverage=true)'
+elif [[ "$TEST_TYPE" == "userimage" ]]; then
+    # Create a user image which includes Memento to make sure that _loggers is assigned at compile-time
+    # See: https://github.com/invenia/Memento.jl/pull/21
+    julia -e '
+        LIB_PATH = abspath(Base.JULIA_HOME, Base.LIBDIR)
+        sysimg_lib = joinpath(LIB_PATH, "julia", "sys.$(Libdl.dlext)")
+        userimg_o = "userimg.o"
+        userimg_lib = "userimg.$(Libdl.dlext)"
+        run(`$(Base.julia_cmd()) --output-o $userimg_o --sysimage $sysimg_lib --startup-file=no -e "using Memento; logger = get_logger(\"Test\")"`)
+        run(`cc -shared -o $userimg_lib $userimg_o -ljulia -L$LIB_PATH`)
+    '
 fi
