805a57ec571fec8227a601047c0d98b223c47427
rofinn <rory.finnegan@gmail.com>
2017-12-06 16:34:38 -0600

    Forgot to update .travis/test.sh file with env change.

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,12 +2,9 @@
 
 set -ev
 
-if [[ "$TEST_TYPE" == "parallel" ]]; then
+if [[ "$TEST_TYPE" == "basic" ]]; then
     # Run parallel tests
     julia -e 'Pkg.test("Memento"; coverage=true)'
-elif [[ "$TEST_TYPE" == "io" ]]; then
-    # Run io tests (with mocking) and coverage
-    julia --compilecache=no -e 'Pkg.test("Memento"; coverage=true)'
 elif [[ "$TEST_TYPE" == "bench" ]]; then
     # Run benchmark tests
     export MEMENTO_BENCHMARK="true"
