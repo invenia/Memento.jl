2e27e4610cfd8ba441f847a555cc40890e319716
Rory Finnegan <rory.finnegan@gmail.com>
2017-12-13 12:37:13 -0600

    Update deploydocs julia version

--- docs/make.jl
+++ docs/make.jl
@@ -36,7 +36,7 @@ makedocs(
 
 deploydocs(
     repo   = "github.com/invenia/Memento.jl.git",
-    julia  = "0.5",
+    julia  = "0.6",
     target = "build",
     deps = nothing,
     make = nothing,
