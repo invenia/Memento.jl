7c6d0f2d9e2bcc27368c86c999cd416f2d5ebad8
Curtis Vogt <curtis.vogt@gmail.com>
2020-03-04 12:28:15 -0600

    Move getindex deprecation

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,3 +1,10 @@
-@deprecate config(args...; kwargs...) config!(args...; kwargs...)
+using Base: @deprecate, @deprecate_binding
+import Base: getindex
 
-Base.@deprecate_binding Test Memento.TestUtils false
+# BEGIN Memento 0.12 deprecations
+
+@deprecate config(args...; kwargs...) config!(args...; kwargs...)
+@deprecate_binding Test Memento.TestUtils false
+@deprecate getindex(rec::T, attr::Symbol) where {T <: Record} getproperty(rec, attr) false
+
+# END Memento 0.12 deprecations
