dc002ca1861c8f2b1a43db3b85b8fc2e13c018c3
rofinn <rory.finnegan@gmail.com>
2020-03-16 12:40:05 -0500

    Drop deprecations and prep 1.0 release.

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,10 +1,1 @@
-using Base: @deprecate, @deprecate_binding
-import Base: getindex
-
-# BEGIN Memento 0.12 deprecations
-
-@deprecate config(args...; kwargs...) config!(args...; kwargs...)
-@deprecate_binding Test Memento.TestUtils false
-@deprecate getindex(rec::T, attr::Symbol) where {T <: Record} getproperty(rec, attr) false
-
-# END Memento 0.12 deprecations
+# Place any deprecations prior to a 2.0 release here
